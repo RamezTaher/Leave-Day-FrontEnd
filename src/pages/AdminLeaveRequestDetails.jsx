ece362b40212c7b90f746a1336df8ff9dea85c05
Ramez Taher <ramezgamer11@gmail.com>
2023-04-09 01:42:00 +0100

    last page and still some fixes

--- src/pages/AdminLeaveRequestDetails.jsx
+++ src/pages/AdminLeaveRequestDetails.jsx
@@ -1,7 +1,19 @@
 import React from "react"
+import Header from "../components/Header"
 
 const AdminLeaveRequestDetails = () => {
-  return <div>AdminLeaveRequestDetails</div>
+  return (
+    <>
+      <Header />
+      <section>
+        <div className="container mx-auto h-full py-10">
+          <div className="space-y-4">
+            <h1 className="text-2xl font-semiBold mb-6">Leave Day Request:</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default AdminLeaveRequestDetails
