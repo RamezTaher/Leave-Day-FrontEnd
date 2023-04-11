2d8c98b9fb046f4d12d5a870bbf76e50d84f9af4
Ramez Taher <ramezgamer11@gmail.com>
2023-04-11 05:53:32 +0100

    add loader to signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -82,98 +82,105 @@ const Signup = () => {
     }
   }
   return (
-    <div className="h-screen flex justify-center items-center bg-gradient-to-r from-blue-500 to-purple-700 z-1 filter ">
-      <div className="text-center ">
-        <h1 className="text-4xl font-semibold mb-4 font-mono text-white">
-          Join ISIMM Leave Day.
-        </h1>
-        {error !== "" && (
-          <p className="text-white font-meduim max-w-sm">{error}</p>
-        )}
+    <>
+      {loading && (
+        <div className="h-screen w-screen bg-black bg-opacity-80 absolute flex items-center justify-center">
+          <Loader />
+        </div>
+      )}
+      <div className="h-screen flex justify-center items-center bg-gradient-to-r from-blue-500 to-purple-700 z-1 filter ">
+        <div className="text-center ">
+          <h1 className="text-4xl font-semibold mb-4 font-mono text-white">
+            Join ISIMM Leave Day.
+          </h1>
+          {error !== "" && (
+            <p className="text-white font-meduim max-w-sm">{error}</p>
+          )}
 
-        <div className="bg-white p-5 text-left rounded-xl mt-1 shadow-lg">
-          <form onSubmit={signupHandler} className="flex flex-col gap-3">
-            <label htmlFor="name">
-              Name
-              <input
-                type="text"
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Name"
-                value={name}
-              />
-            </label>
-            <label htmlFor="email">
-              Email
-              <input
-                type="text"
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-                value={email}
-              />
-            </label>
-            <label htmlFor="phone">
-              Phone
-              <input
-                type="text"
-                onChange={(e) => setPhone(e.target.value)}
-                placeholder="Phone"
-                value={phone}
-              />
-            </label>
-            <label htmlFor="department">
-              Department
-              <select onChange={(e) => setDepartment(e.target.value)}>
-                {departments.map((department, idx) => (
-                  <option key={idx} value={department._id}>
-                    {department.name}
-                  </option>
-                ))}
-              </select>
-            </label>
-
-            <label htmlFor="password" className="relative">
-              Password
-              <input
-                type={showPassword ? "text" : "password"}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-                value={password}
-              />
-              {showPassword ? (
-                <AiOutlineEyeInvisible
-                  onClick={() => setShowPassword(false)}
-                  className="text-2xl cursor-pointer absolute right-3 top-1/2 translate-y-[2px] "
+          <div className="bg-white p-5 text-left rounded-xl mt-1 shadow-lg">
+            <form onSubmit={signupHandler} className="flex flex-col gap-3">
+              <label htmlFor="name">
+                Name
+                <input
+                  type="text"
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Name"
+                  value={name}
                 />
-              ) : (
-                <AiOutlineEye
-                  onClick={() => setShowPassword(true)}
-                  className="text-2xl cursor-pointer absolute right-3 top-1/2 translate-y-[2px]"
+              </label>
+              <label htmlFor="email">
+                Email
+                <input
+                  type="text"
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  value={email}
                 />
-              )}
-            </label>
+              </label>
+              <label htmlFor="phone">
+                Phone
+                <input
+                  type="text"
+                  onChange={(e) => setPhone(e.target.value)}
+                  placeholder="Phone"
+                  value={phone}
+                />
+              </label>
+              <label htmlFor="department">
+                Department
+                <select onChange={(e) => setDepartment(e.target.value)}>
+                  {departments.map((department, idx) => (
+                    <option key={idx} value={department._id}>
+                      {department.name}
+                    </option>
+                  ))}
+                </select>
+              </label>
 
-            <label htmlFor="confirm-password">
-              Password
-              <input
-                type={showPassword ? "text" : "password"}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                placeholder="Password"
-                value={confirmPassword}
-              />
-            </label>
+              <label htmlFor="password" className="relative">
+                Password
+                <input
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  value={password}
+                />
+                {showPassword ? (
+                  <AiOutlineEyeInvisible
+                    onClick={() => setShowPassword(false)}
+                    className="text-2xl cursor-pointer absolute right-3 top-1/2 translate-y-[2px] "
+                  />
+                ) : (
+                  <AiOutlineEye
+                    onClick={() => setShowPassword(true)}
+                    className="text-2xl cursor-pointer absolute right-3 top-1/2 translate-y-[2px]"
+                  />
+                )}
+              </label>
 
-            <div className="flex justify-center">
-              <button
-                type="submit"
-                className="bg-blue-500 text-white btn px-3 py-2 rounded-md mt-4  text-lg font-semibold shadow-md"
-              >
-                Signup
-              </button>
-            </div>
-          </form>
+              <label htmlFor="confirm-password">
+                Password
+                <input
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  placeholder="Password"
+                  value={confirmPassword}
+                />
+              </label>
+
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="bg-blue-500 text-white btn px-3 py-2 rounded-md mt-4  text-lg font-semibold shadow-md"
+                >
+                  Signup
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
